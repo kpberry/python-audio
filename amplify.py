46d77fbea834a121d782cd9e5ca0d199d7bf622a
Kevin Berry <kpberry11@gmail.com>
2021-05-24 11:37:07 -0400

    Adds blend function

--- amplify.py
+++ amplify.py
@@ -6,4 +6,8 @@ def amplify(samples, scale=1):
 
 
 def normalize(samples, new_peak=1):
-    return samples * new_peak / np.max(np.abs(samples))
+    return amplify(samples, new_peak / np.max(np.abs(samples)))
+
+
+def blend(samples_a, samples_b, ratio=0.5):
+    return samples_a * ratio + samples_b * (1 - ratio)
